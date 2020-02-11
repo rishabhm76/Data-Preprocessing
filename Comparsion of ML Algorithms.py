5f258478f95ee8a277e5ff753adbf0035b928521
Rishabh Malhotra <49758122+rishabhm76@users.noreply.github.com>
2020-02-12 01:03:55 +0530

    Add files via upload

---
+++ Comparsion of ML Algorithms.py
@@ -0,0 +1,56 @@
+# import libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# load dataset
+
+dataframe = pd.read_csv('Pima Indians Diabetes Database.csv')
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+
+# prepare configuration for cross validation test harness
+
+seed = 7
+
+# prepare models
+
+models = []
+models.append(('LR', LinearRegression()))
+models.append(('LogR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('DT', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+# boxplot algorithm comparison
+
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
